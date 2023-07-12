011edef342a07d0a0c4aee002c59af20459d4772
geon3017 <geon3017@naver.com>
2023-07-12 15:33:51 +0900

    REAMD 최건 230712 거의 완료

--- reference.js
+++ reference.js
@@ -16,6 +16,9 @@ function scrollRotate() {
   // 문서가 현재 세로축을 따라 스크롤되는 픽셀수를 반환하는 Window.pageYOffset 속성을 적용 합니다.
   purple.style.transform = 'rotate(' + window.pageYOffset / 15 + 'deg)';
 }
+
+//****************************************************************************** */
+
 // header - sub-menu
 let prevScroll = window.scrollY;
 console.log(prevScroll);
@@ -29,11 +32,11 @@ $(window).on('scroll', () => {
   // 이전 스크롤 값이 크면 true, 스크롤을 올리면 header 보임
   if (prevScroll > currentScroll) {
     // $('header').show();
-    $('.sub-menu').css({ top: 0 });
+    $('.header').css({ top: 0 });
   } else {
     // 이후 스크롤 값이 크면 false, 스크롤을 내리면 header 숨김
     // $('header').hide();
-    $('.sub-menu').css({ top: -150 });
+    $('.header').css({ top: -150 });
   }
 
   // prev에 현재 스크롤 값 대입
@@ -41,15 +44,25 @@ $(window).on('scroll', () => {
   console.log('2prevScroll', prevScroll);
 });
 
+//************************************************************ */
+
 // menu 버튼
-$('#openbtn').on('click', () => {
-  $('.menubox').css({ opacity: 1 }).animate({ top: 0 }, 300);
-  $('#openbtn').hide();
+$(function () {
+  $('.checkbox').on('click', () => {
+    $('.hamburger_btn').toggleClass('active');
+    $('.menu').slideToggle(); // == display: block
+  });
 });
-$('#closebtn').on('click', () => {
-  $('.menubox').animate({ top: 1500 }, 300);
-  $('#openbtn').show();
+
+$('#project').on('click', () => {
+  $('.menu').slideToggle();
 });
+
+$('#about').on('click', () => {
+  $('.menu').slideToggle();
+});
+
+//***************************************************************** */
 
 // .projects .project-menu 프로젝트 부분
 $(document).ready(function () {
@@ -65,22 +78,9 @@ $(document).ready(function () {
   });
 });
 
-// $(document).ready(function() {
-//     $(window).scroll( function(){
-//         $('.projects .project-menu .row-reverse').each( function(i){
-
-//             var bottom_of_element = $(this).offset().top + $(this).outerHeight();
-//             var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-//             if( bottom_of_window > bottom_of_element ){
-//                 $(this).animate({'opacity':'1','margin-right':'0px'},1000);
-//             }
-
-//         });
-//     });
-// });
-
+//********************************************************************** */
 // .projects .expl 목차부분
+
 $(document).ready(function () {
   $(window).scroll(function () {
     $('.projects .expl').each(function (i) {
@@ -94,32 +94,11 @@ $(document).ready(function () {
   });
 });
 
+//************************************************************************* */
+
+//**************************************************************************** */
+
 // 롤링
-// function movingLetter(n) {
-//     const txt = $('.aniText');
-//     const currentTxt = txt.eq(n);
-//     const nextIdx = (n + 1) % txt.length;
-
-//     txt.removeClass('active');
-//     currentTxt.addClass('active');
-
-//     setTimeout(function () {
-//       movingLetter(nextIdx);// 재귀함수
-//     }, 2000);
-//   }
-
-//   let num = 0;
-//   movingLetter(num);
-// setInterval(function(){
-//     $('#slide2>ul').delay(3000);
-//     $('#slide2>ul').animate({marginTop: "-200px"})
-//     $('#slide2>ul').delay(3000);
-//     $('#slide2>ul').animate({marginTop: "-400px"})
-//     $('#slide2>ul').delay(3000);
-//     $('#slide2>ul').animate({marginTop: "0px"})
-// });
-// $(document).ready(function(){
-
 jQuery(function ($) {
   var ticker = function () {
     timer = setTimeout(function () {
